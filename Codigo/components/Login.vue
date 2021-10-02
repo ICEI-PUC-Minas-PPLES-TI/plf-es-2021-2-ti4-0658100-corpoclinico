66f03cce2bbd55abae5da3dc258c558632bfdd6c
Vinícius Marini <marini_costa@hotmail.com>
2021-10-02 15:32:16 -0300

    handleSubmit

--- Codigo/components/Login.vue
+++ Codigo/components/Login.vue
@@ -7,12 +7,12 @@
                         <v-card elevation="0">
                             <v-img src="@/assets/logo.png" alt="corpo clinico" contain height="200"></v-img>
                             <v-card-text>
-                                <v-form>
-                                    <v-text-field label="Login" name="login" prepend-inner-icon="mdi-account-circle" type="email" class="rounded-0" outlined placeholder="email@email.com" v-model="login"></v-text-field>
+                                <v-form @submit.prevent="handleSubmit">
+                                    <v-text-field label="Login" name="email" prepend-inner-icon="mdi-account-circle" type="email" class="rounded-0" outlined placeholder="email@email.com" v-model="email"></v-text-field>
                                     <v-text-field label="Senha" name="senha" prepend-inner-icon="mdi-lock" type="senha" outlined class="rounded-0" v-model="senha"></v-text-field>
                                      <v-checkbox color="#25BAAE" label="Lembrar-me" class="mt-0">
                                     </v-checkbox>
-                                    <v-btn  @click.native="logar" class="rounded-0" color="#25BAAE" x-large block dark > Entrar </v-btn>
+                                    <v-btn  @click.native="handleSubmit" class="rounded-0" color="#25BAAE" x-large block dark > Entrar </v-btn>
                                     <v-card-actions class="text--secondary" >
                                         Ainda não se candidatou?<a href="#" class="pl-2" style="color: #25BAAE">Candidatar-se</a>
                                     </v-card-actions>
@@ -27,25 +27,25 @@
 </template>
 
 <script>
+    import axios from 'axios';
 export default {
+    name: 'Login',
     data(){
         return{
-            login:'',
-            senha:'',
+            email:'',
+            senha:''
         }
     },
-    methods:{
-        logar() {
-            let hoje = new Date();
-            console.log(hoje.getDate())
-
-            document.cookie = `login=${this.login}; expires= Thu, 21 Aug 2022 20:00:00 UTC`
-        },
-        valida(){
-
+    methods: {
+        async handleSubmit(){
+            const response= await axios.post('http://localhost:3000/api/signin',{
+                email:this.email,
+                password: this.password
+            });
+            console.log(response);
         }
     },
 }
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped></style>'
