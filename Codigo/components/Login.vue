f9df125ca865afd4fd676ef5b7c90e9a89ee0b8e
Vinícius Marini <marini_costa@hotmail.com>
2021-10-02 11:07:58 -0300

    editei icone inicio cookie

--- Codigo/components/Login.vue
+++ Codigo/components/Login.vue
@@ -1,19 +1,18 @@
 <template>
     <v-app>
         <v-main>
-            <v-container class="fill-height" fluid >
+            <v-container class="fill-height" fluid secondary>
                 <v-row align="center" justify="center" dense>
-                    <v-col cols="12" sm="8" md="4" lg="4">
+                    <v-col cols="12" sm="8" md="5" lg="4">
                         <v-card elevation="0">
-
                             <v-img src="@/assets/logo.png" alt="corpo clinico" contain height="200"></v-img>
                             <v-card-text>
                                 <v-form>
-                                    <v-text-field label="Login" name="email" prepend-inner-icon="mdi-account-circle" type="email" class="rounded-0" outlined placeholder="email@email.com"></v-text-field>
-                                    <v-text-field label="Senha" name="password" prepend-inner-icon="mdi-lock" type="password" outlined class="rounded-0"></v-text-field>
+                                    <v-text-field label="Login" name="login" prepend-inner-icon="mdi-account-circle" type="email" class="rounded-0" outlined placeholder="email@email.com" v-model="login"></v-text-field>
+                                    <v-text-field label="Senha" name="senha" prepend-inner-icon="mdi-lock" type="senha" outlined class="rounded-0" v-model="senha"></v-text-field>
                                      <v-checkbox color="#25BAAE" label="Lembrar-me" class="mt-0">
                                     </v-checkbox>
-                                    <v-btn  class="rounded-0" color="#25BAAE" x-large block dark> Entrar </v-btn>
+                                    <v-btn  @click.native="logar" class="rounded-0" color="#25BAAE" x-large block dark > Entrar </v-btn>
                                     <v-card-actions class="text--secondary" >
                                         Ainda não se candidatou?<a href="#" class="pl-2" style="color: #25BAAE">Candidatar-se</a>
                                     </v-card-actions>
@@ -29,7 +28,23 @@
 
 <script>
 export default {
-    
+    data(){
+        return{
+            login:'',
+            senha:'',
+        }
+    },
+    methods:{
+        logar() {
+            let hoje = new Date();
+            console.log(hoje.getDate())
+
+            document.cookie = `login=${this.login}; expires= Thu, 21 Aug 2022 20:00:00 UTC`
+        },
+        valida(){
+
+        }
+    },
 }
 </script>
 
