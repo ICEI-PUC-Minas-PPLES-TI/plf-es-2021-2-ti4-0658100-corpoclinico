403ba6a6e93cb7290ec01bee071f063f356b01cb
Vinícius Marini <marini_costa@hotmail.com>
2021-10-06 22:30:29 -0300

    centralizado

--- Codigo/components/Login.vue
+++ Codigo/components/Login.vue
@@ -4,11 +4,13 @@
             <v-container class="fill-height" fluid secondary>
                 <v-row align="center" justify="center" dense>
                     <v-col cols="12" sm="8" md="5" lg="4">
-                        <v-card class="py-2" elevation="0">
-                            <v-img  src="@/assets/logo.svg" alt="corpo clinico" contain height="150" width="400"></v-img>
+                        <v-card class="py-4" elevation="0">
+                        <v-row align="center" justify="center">
+                            <v-img  max-width="40%" src="@/assets/logo.svg" alt="corpo clinico"></v-img>
+                        </v-row>
                             <v-card-text>
                                 <v-form @submit.prevent="handleSubmit">
-                                    <v-text-field label="Login" name="email" prepend-inner-icon="mdi-account-circle" type="email" class="rounded-0" outlined placeholder="email@email.com" v-model="email"></v-text-field>
+                                    <v-text-field label="Login" name="email" prepend-inner-icon="mdi-account-circle" type="email" class="p-0rounded-0" outlined placeholder="email@email.com" v-model="email"></v-text-field>
                                     <v-text-field label="Senha" name="senha" prepend-inner-icon="mdi-lock" type="senha" outlined class="rounded-0" v-model="senha"></v-text-field>
                                      <v-checkbox color="#25BAAE" label="Lembrar-me" class="mt-0">
                                     </v-checkbox>
@@ -40,12 +42,13 @@ export default {
         async handleSubmit(){
             // const response= await axios.post('http://localhost:3000/api/signin',{
             //     email:this.email,
-            //     senha: this.senha
+            //     password: this.password
             // });
-            console.log(this.email,this.senha);
+            // console.log(response);
+            console.log(this.email + ' ' + this.senha)
         }
     },
 }
 </script>
 
-<style lang="css" scoped></style>'
+<style lang="css" scoped></style>
