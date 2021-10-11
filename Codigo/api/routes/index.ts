d77403213ff1f6ea8948ebe7e70a0a0be2e86234
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-10-11 19:21:20 -0300

    Atualizando nomes de arquivos

--- Codigo/api/routes/index.ts
+++ Codigo/api/routes/index.ts
@@ -6,7 +6,7 @@ import autenticacaoJwt from './verificarJwtToken';
 // Importar controllers
 import UsuarioController from '../controllers/UsuarioController';
 import MedicoController from '../controllers/MedicoController';
-import { upload } from '../helpers/files/Multer';
+import { upload } from '../helpers/files/Multer'; 
 import { documentosCriarMedico } from '../helpers/files/Documentos'; 
 
 // Iniciar controllers
