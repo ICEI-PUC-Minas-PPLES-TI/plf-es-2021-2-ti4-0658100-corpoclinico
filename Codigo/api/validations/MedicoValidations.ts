b84e78f6c1fca21ac23a54fa9cbb93024c7df794
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-10-12 11:51:25 -0300

    Adicionando nullable

--- Codigo/api/validations/MedicoValidations.ts
+++ Codigo/api/validations/MedicoValidations.ts
@@ -138,85 +138,85 @@ export const medicoCreateValidationScheme = yup.object().shape({
 });
 
 export const medicoUpdateValidationScheme = yup.object().shape({
-  crm: yup.string().max(20, "'crm' tamanho máximo de 20 caracteres!"),
+  crm: yup.string().max(20, "'crm' tamanho máximo de 20 caracteres!").nullable(),
 
   regiao: yup
     .mixed()
-    .oneOf(estados, `'regiao' deve ser algum destes valores: ${estados}!`),
+    .oneOf(estados, `'regiao' deve ser algum destes valores: ${estados}!`).nullable(),
 
-  dt_inscricao_crm: yup.date(),
+  dt_inscricao_crm: yup.date().nullable(),
 
   celular: yup
     .string()
     .matches(celularRegEx, "'celular' inválido!")
-    .max(14, "'celular' tamanho máximo de 14 caracteres!"),
+    .max(14, "'celular' tamanho máximo de 14 caracteres!").nullable(),
 
   cartao_sus: yup
     .string()
-    .max(25, "'cartao_sus' tamanho máximo de 25 caracteres!"),
+    .max(25, "'cartao_sus' tamanho máximo de 25 caracteres!").nullable(),
 
   categoria: yup
     .mixed()
     .oneOf(
       categorias,
       `'categoria' deve ser algum destes valores: ${categorias}.`
-    ),
+    ).nullable(),
 
-  rg: yup.string().max(14, "'rg' tamanho máximo de 14 caracteres!"),
+  rg: yup.string().max(14, "'rg' tamanho máximo de 14 caracteres!").nullable(),
 
   rg_orgao_emissor: yup
     .string()
-    .max(30, "'rg_orgao_emissor' tamanho máximo de 30 caracteres!"),
+    .max(30, "'rg_orgao_emissor' tamanho máximo de 30 caracteres!").nullable(),
 
-  rg_data_emissao: yup.date(),
+  rg_data_emissao: yup.date().nullable(),
 
-  dt_nascimento: yup.date(),
+  dt_nascimento: yup.date().nullable(),
 
   cpf: yup
     .string()
     .matches(cpfRegEx, "'cpf' inválido!")
-    .max(14, "'cpf' tamanho máximo de 14 caracteres!"),
+    .max(14, "'cpf' tamanho máximo de 14 caracteres!").nullable(),
 
   titulo_eleitoral: yup
     .string()
     .min(1, "'titulo_eleitoral' tamanho mínimo de 1 caractere.")
-    .max(12, "'titulo_eleitoral' tamanho máximo de 12 caracteres!"),
+    .max(12, "'titulo_eleitoral' tamanho máximo de 12 caracteres!").nullable(),
 
-  zona: yup.string().max(3, "'zona' tamanho máximo de 3 caracteres!"),
+  zona: yup.string().max(3, "'zona' tamanho máximo de 3 caracteres!").nullable(),
 
-  secao: yup.string().max(4, "'secao' tamanho máximo de 4 caracteres!"),
+  secao: yup.string().max(4, "'secao' tamanho máximo de 4 caracteres!").nullable(),
 
   logradouro: yup
     .string()
-    .max(100, "'logradouro' tamanho máximo de 100 caracteres!"),
+    .max(100, "'logradouro' tamanho máximo de 100 caracteres!").nullable(),
 
-  numero: yup.number(),
+  numero: yup.number().nullable(),
 
   complemento: yup
     .string()
-    .max(20, "'complemento' tamanho máximo de 20 caracteres!"),
+    .max(20, "'complemento' tamanho máximo de 20 caracteres!").nullable(),
 
-  bairro: yup.string().max(45, "'bairro' tamanho máximo de 45 caracteres!"),
+  bairro: yup.string().max(45, "'bairro' tamanho máximo de 45 caracteres!").nullable(),
 
-  cidade: yup.string().max(100, "'cidade' tamanho máximo de 100 caracteres!"),
+  cidade: yup.string().max(100, "'cidade' tamanho máximo de 100 caracteres!").nullable(),
 
   estado: yup
     .mixed()
-    .oneOf(estados, `'estado' deve ser algum destes valores: ${estados}!`),
+    .oneOf(estados, `'estado' deve ser algum destes valores: ${estados}!`).nullable(),
 
   cep: yup
     .string()
     .matches(cepRegEx, "'cep' inválido!")
-    .max(9, "'cep' tamanho máximo de 9 caracteres!"),
+    .max(9, "'cep' tamanho máximo de 9 caracteres!").nullable(),
 
   sociedade_cientifica: yup
     .string()
-    .max(100, "'sociedade_cientifica' tamanho máximo de 100 caracteres!"),
+    .max(100, "'sociedade_cientifica' tamanho máximo de 100 caracteres!").nullable(),
 
   escolaridade_max: yup
     .mixed()
     .oneOf(
       escolaridades,
       `'escolaridade_max' deve ser algum destes valores: ${escolaridades}!`
-    )
+    ).nullable(),
 });
