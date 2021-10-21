fb33560870d8515deb1f69df8f56a2e75dc40970
guizombas <guilherme.gabriel.532@gmail.com>
2021-10-20 21:09:50 -0300

    correcao npm run api

--- Codigo/api/index.ts
+++ Codigo/api/index.ts
@@ -43,3 +43,12 @@ app.use([(err, request, response, next) => {
 }]);
 
 export default app;
+
+if (require.main === module) {
+  app.use('/api', routes);
+  const port = process.env.PORT || 3001;
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`API server listening on port ${port}`);
+  });
+}
