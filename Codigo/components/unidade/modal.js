23f59d3305cfffc818c7b52580d86eeef4a75dec
Vinícius Marini <marini_costa@hotmail.com>
2021-11-01 09:18:30 -0300

    edição seguida funcionando

--- Codigo/components/unidade/modal.js
+++ Codigo/components/unidade/modal.js
@@ -87,11 +87,10 @@ export default {
         let unidade = JSON.parse(JSON.stringify(this.unidade));
         
         this.$axios.$put('/unidade/' + id, unidade).then(response => {
-          this.limpaDados();
+          this.$emit('listaUnidade')
           this.abreToast('Unidade Atualizada!');
           this.$emit('input', false) // Fecha modal
-
-        }).catch(error => {
+          }).catch(error => {
           if(Array.isArray(error.response.data.erros)){
             this.abreToast(error.response.data.erros[0]);
           }else{
