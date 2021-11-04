ef0cf30e31776103330884f1740a65d112512a51
guizombas <guilherme.gabriel.532@gmail.com>
2021-11-03 23:45:25 -0300

    padrao em erros de auth

--- Codigo/api/routes/index.ts
+++ Codigo/api/routes/index.ts
@@ -32,7 +32,7 @@ router.delete('/usuario/:id', [autenticacaoJwt.verificarToken, autenticacaoJwt.i
 router.put('/usuario/:id', [autenticacaoJwt.verificarToken, autenticacaoJwt.isAdmin], usuarioController.update)
 // Medico
 router.post('/medico', multerUploadCriarMedico, medicoController.create)
-router.get('/medico/:id', [autenticacaoJwt.verificarToken], medicoController.get)
+router.get('/medico/:id', [autenticacaoJwt.verificarToken, autenticacaoJwt.isAdmin], medicoController.get)
 router.get('/medico', [autenticacaoJwt.verificarToken], medicoController.getAll)
 router.delete('/medico/:id', [autenticacaoJwt.verificarToken, autenticacaoJwt.isAdmin], medicoController.delete)
 router.put('/medico/:id', [autenticacaoJwt.verificarToken, autenticacaoJwt.isAdmin], medicoController.update)
