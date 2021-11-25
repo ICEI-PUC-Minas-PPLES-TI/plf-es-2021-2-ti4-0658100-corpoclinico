3dc78493d00cd8e3befbbdc81a7e60f75ba9ab1f
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-11-25 19:45:03 -0300

    Validando link

--- Codigo/api/validations/VideoValidations.ts
+++ Codigo/api/validations/VideoValidations.ts
@@ -1,12 +1,12 @@
 import * as yup from 'yup'
 
 export const videoCreateValidationScheme = yup.object().shape({
-    link: yup.string().max(250).required(),
+    link: yup.string().url().max(250).required(),
     prioridade: yup.number().max(127).required(),
 });
 
 export const videoUpdateValidationScheme = yup.object().shape({
-    link: yup.string().max(250),
+    link: yup.string().url().max(250),
     prioridade: yup.number().max(127),
     ativo: yup.bool()
 });
