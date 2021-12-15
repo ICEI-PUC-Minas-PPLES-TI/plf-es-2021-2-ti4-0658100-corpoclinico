0bad95245c47ef6b8f3110b6e6586c79d6afcc3b
Lucas Ângelo O. M. Rocha <49598959+Lucas-Angelo@users.noreply.github.com>
2021-12-15 18:20:13 -0300

    Nullable

--- Codigo/api/validations/RetornoValidations.ts
+++ Codigo/api/validations/RetornoValidations.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup'
 
 export const retornoUpdateScheme = yup.object().shape({
-    comentario: yup.string(),
+    comentario: yup.string().nullable(),
     status: yup.mixed().oneOf(['P', 'R', 'A']).required()
 });
